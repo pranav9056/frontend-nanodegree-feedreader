e00558aeab5f2519c012452eef753568979c88e0
Pranav <pranavjain3194@gmail.com>
2018-05-14 11:20:50 -0400

    fead: added spec - have valid URL to RSS Feeds

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,11 +26,19 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
-        /* TODO: Write a test that loops through each feed
+        /* a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+
+        it("have valid URLS",function(){
+          allFeeds.forEach(function(feed){
+            expect(feed.url).toBeDefined();
+            expect(feed.url).not.toBe("");
+          });
+        });
+
+
 
 
         /* TODO: Write a test that loops through each feed
